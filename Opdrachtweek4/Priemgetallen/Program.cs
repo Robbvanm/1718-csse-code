1bc72c3fdc1f24801234edea502a7cd57ea0134f
robbvanm <robbvanm@student.arteveldehs.be>
2017-10-23 17:12:27 +0200

    Opdacht week 4
    fibonacci en priemgetallen

---
+++ Opdrachtweek4/Priemgetallen/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Priemgetallen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal, teller, aantal = 0, deler = 1;
+
+            Console.Write("Input(max 100): ");
+            getal = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Output: ");
+            Console.WriteLine();
+            if(getal <= 100)
+            {
+                for(teller = 1; teller <= getal; teller++)
+                {
+                    Console.Write(teller.ToString() + " ");
+                    aantal = 0;
+
+                    for(deler = 1; deler <= teller; deler++)
+                    {
+                        if(teller % deler == 0)
+                        {
+
+                            aantal++;
+                        }
+                    }
+                    if(aantal == 2)
+                    {
+                        Console.Write("is een priemgetal");
+                    }
+                    else{
+                        Console.Write("ss geen priemgetal");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            } 
+            else
+            {
+                {
+                    Console.WriteLine("Getal moet onder de 100 zijn");
+                }
+            }
+        }
+    }
+}
---
+++ Opdrachtweek4/RijFibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RijFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int teller, getal, getal1 = 0, getal2 = 1, getal3;
+
+            Console.Write("input: ");
+            getal = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Output: ");
+            Console.WriteLine();
+            Console.Write(getal1.ToString());
+            Console.WriteLine();
+            Console.Write(getal2.ToString());
+            Console.WriteLine();
+            for(teller = 3; teller <=getal; teller++)
+            {
+                getal3 = getal1 + getal2;
+                getal1 = getal2;
+                getal2 = getal3;
+
+                Console.Write(getal3.ToString() + " ");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
